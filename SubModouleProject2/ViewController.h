94c0819879404afe2435b42cc0ff16b2489846ec
Done.L <liudongdong@zjlp.com>
2017-09-11 13:56:07 +0800

    Initial Commit

---
+++ SubModouleProject2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SubModouleProject2
+//
+//  Created by Done.L on 2017/9/11.
+//  Copyright © 2017年 Done.L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SubModouleProject2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SubModouleProject2
+//
+//  Created by Done.L on 2017/9/11.
+//  Copyright © 2017年 Done.L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
